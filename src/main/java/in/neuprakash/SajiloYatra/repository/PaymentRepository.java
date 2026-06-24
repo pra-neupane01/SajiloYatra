d2c634845af7501555360b3a05a4188ea3974942
prakash69-code <neupaneprakash9999@gmail.com>
2026-06-24 22:25:08 +0545

    chore: fix repository for payment

--- src/main/java/in/neuprakash/SajiloYatra/repository/PaymentRepository.java
+++ src/main/java/in/neuprakash/SajiloYatra/repository/PaymentRepository.java
@@ -3,7 +3,11 @@ package in.neuprakash.SajiloYatra.repository;
 import in.neuprakash.SajiloYatra.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     boolean existsByBookingId(Long bookingId);
+
+    Optional<Payment> findByBookingId(Long bookingId);
 }
