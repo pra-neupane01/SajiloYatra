f5fc96609e1ffee842ad79f4d3080da3ec8ab441
prakash69-code <neupaneprakash9999@gmail.com>
2026-06-24 22:24:16 +0545

    chore: fix repository for ticket

--- src/main/java/in/neuprakash/SajiloYatra/repository/TicketRepository.java
+++ src/main/java/in/neuprakash/SajiloYatra/repository/TicketRepository.java
@@ -3,5 +3,11 @@ package in.neuprakash.SajiloYatra.repository;
 import in.neuprakash.SajiloYatra.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    boolean existsByBookingId(Long bookingId);
+
+    Optional<Ticket> findByBookingId(Long bookingId);
 }
